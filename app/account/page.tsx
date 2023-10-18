48b48cfbd30634ff682937761d312ef4401d7eda
stackrev <farbitherast5888@gmail.com>
2023-10-18 07:37:20 -0400

    refactor the Account page code

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -11,14 +11,10 @@ import { revalidatePath } from 'next/cache';
 import { cookies } from 'next/headers';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
-import { ReactNode } from 'react';
 
-import { sendUSDC } from '@/utils/usdc';
+// import { loadStripeOnramp } from '@stripe/crypto';
+// import { CryptoElements, OnrampElement } from '@/components/StripeCryptoElements';
 
-import { loadStripeOnramp } from '@stripe/crypto';
-
-import { CryptoElements, OnrampElement } from '@/components/StripeCryptoElements';
-import CopyableAddress from '@/components/CopyableAddress';
 import AccountData from '@/components/account/AccountData';
 import WithMetamask from '@/components/account/WithMetamask';
 import WagmiConfigProvider from '@/components/account/WagmiConfigProvider';
@@ -26,22 +22,23 @@ import Withdraw from '@/components/account/Withdraw';
 import Invest from '@/components/account/Invest';
 import UnInvest from '@/components/account/UnInvest';
 
-const stripeOnrampPromise = loadStripeOnramp("pk_test_51NtMMKAo4s8oHTt3DNSMT4ReRVNVIjWni3m5muZe6ldTk3iN3GuRBWQvOVJ5VgAxLMtJvKDaKeimqTlazkkbl9N600CwO4E3FF");
+// const stripeOnrampPromise = loadStripeOnramp("pk_test_51NtMMKAo4s8oHTt3DNSMT4ReRVNVIjWni3m5muZe6ldTk3iN3GuRBWQvOVJ5VgAxLMtJvKDaKeimqTlazkkbl9N600CwO4E3FF");
 
 // export const revalidate = 0
 
 export default async function Account() {
   // IMPORTANT: replace with your logic of how to mint/retrieve client secret
-  const clientSecret = "cos_1NvOGwAo4s8oHTt3u3rz3na6_secret_EbBDNAcASzNzlEcXRWUS31WCU00rrGoRtfi";
+  // const clientSecret = "cos_1NvOGwAo4s8oHTt3u3rz3na6_secret_EbBDNAcASzNzlEcXRWUS31WCU00rrGoRtfi";
   const [session] = await Promise.all([
     getSession()
   ]);
 
+  const user = session?.user;
+  const [userDetail] = user ? await Promise.all([getUserDetail(user.id)]) : [];
+
   if (!session) {
     return redirect('/signin');
   }
-  const user = session?.user;
-  const [userDetail] = user ? await Promise.all([getUserDetail(user.id)]) : [];
 
   return (
     <section className="mb-4 bg-black">
@@ -56,13 +53,6 @@ export default async function Account() {
         <div className="p-4">
           <div className="sm:mx-auto sm:w-full sm:max-w-4xl">
             <AccountData userDetail={userDetail} email={user?.email || ""} />
-
-            {/* <div className="mt-6 w-full bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-
-              <Link href="/withdraw" className="flex w-full bg-blue-500 text-white justify-center py-2 mt-2 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-                Withdraw
-              </Link>
-            </div> */}
             <div className="flex flex-col sm:flex-row sm:space-x-3 ">
               <div className='sm:w-1/2 sm:pr-3'>
                 <WagmiConfigProvider>
