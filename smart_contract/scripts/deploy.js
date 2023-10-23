1e89a4396aab079612b270327e6b3a83719e339f
stackrev <farbitherast5888@gmail.com>
2023-10-23 10:05:43 -0400

    sending 0.01 eth for a new user

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -39,7 +39,7 @@ async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const lockedAmount = hre.ethers.parseEther("0.01");
 
   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
     value: lockedAmount,
